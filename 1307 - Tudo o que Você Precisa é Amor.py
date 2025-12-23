9938dc566e1ecfea22ea9f53ee9ed53fb93e2284
João <jp.estudos17@gmail.com>
2025-12-23 16:26:12 -0300

    Beecrowd

--- 1307 - Tudo o que Você Precisa é Amor.py
+++ 1307 - Tudo o que Você Precisa é Amor.py
@@ -1,11 +1,18 @@
-import math
+def mdc(a, b):
+    if b == 0:
+        return a
+    else:
+        return mdc(b, a % b)
+
+
 n = int(input())
 for i in range(1, n+1):
-    #recebendo o valor e transformando em binário
+    # recebendo o valor e transformando em binário
     n1 = int(input(), 2)
     n2 = int(input(), 2)
-    #mdc de n1 e n2
-    g = math.gcd(n1, n2)
+    # mdc de n1 e n2
+    g = mdc(n1, n2)
+
     if g > 1:
         print(f"Pair #{i}: All you need is love!")
     else:
