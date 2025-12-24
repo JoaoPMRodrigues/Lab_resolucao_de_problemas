de9eba320c80229a81a6b2d00a3d3d8e1eea4559
João <jp.estudos17@gmail.com>
2025-12-23 23:42:53 -0300

    Beecrowd

--- 1040 - Média 3.py
+++ 1040 - Média 3.py
@@ -1,7 +1,7 @@
-n1, n2, n3, n4 = map(float(int, input().split()))
-
+n1, n2, n3, n4 = map(float, input().split())
 media = (n1*2 + n2*3 + n3*4 + n4)/10
 print(f"Media: {media:.1f}")
+
 if media >= 7:
     print("Aluno aprovado.")
 elif media < 5:
@@ -10,9 +10,9 @@ else:
     nr = float(input())
     nova_media = (media+nr)/2
     print("Aluno em exame.")
-    print(f"Nota do exame: {nova_media:.1f}")
+    print(f"Nota do exame: {nr:.1f}")
     if nova_media >= 5:
-        print("Aluno aprovado")
+        print("Aluno aprovado.")
     else:
         print("Aluno reprovado.")
-    print(f"Media final: {nova_media}")
+    print(f"Media final: {nova_media:.1f}")
