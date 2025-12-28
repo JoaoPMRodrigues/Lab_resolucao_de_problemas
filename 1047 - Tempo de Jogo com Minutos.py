cac8d54d577e4cf0888da48c1320f63cd3f6e7e6
João Paulo Medeiros Rodrigues <jp.estudos17@gmail.com>
2025-12-28 08:13:07 -0300

    Beecrowd

--- 1047 - Tempo de Jogo com Minutos.py
+++ 1047 - Tempo de Jogo com Minutos.py
@@ -1,17 +1,13 @@
 hora_inicial, minuto_inicial, hora_final, minuto_final = map(
     int, input().split())
-if hora_inicial == hora_final:
+if hora_inicial == hora_final and minuto_inicial == minuto_final:
     horas = 24
-elif hora_inicial < hora_final:
-    horas = hora_final - hora_inicial
-else:
-    horas = hora_inicial + hora_final - 24
-if minuto_inicial == minuto_final:
     minutos = 0
-elif minuto_inicial < minuto_final:
-    minutos = minuto_final - minuto_inicial
-else:
+elif hora_inicial < hora_final and minuto_inicial > minuto_final:
+    horas = hora_final - (hora_inicial + 1)
     minutos = minuto_inicial + minuto_final - 60
-    horas += 1
+elif hora_inicial < hora_final and minuto_inicial < minuto_final:
+    horas = hora_final - hora_inicial
+    minutos = minuto_inicial - minuto_final
 
 print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
