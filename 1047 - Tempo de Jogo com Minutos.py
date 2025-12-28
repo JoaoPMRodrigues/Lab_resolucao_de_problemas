e4f696e9832dc9597c9cfddec5c1125b2d88ea27
João Paulo Medeiros Rodrigues <jp.estudos17@gmail.com>
2025-12-28 08:40:24 -0300

    Becrowd

--- 1047 - Tempo de Jogo com Minutos.py
+++ 1047 - Tempo de Jogo com Minutos.py
@@ -1,23 +1,17 @@
 hora_inicial, minuto_inicial, hora_final, minuto_final = map(
     int, input().split())
-# Horas iguais
-if hora_inicial == hora_final and minuto_inicial == minuto_final:
+tempo_inicial = hora_inicial * 60 + minuto_inicial
+tempo_final = hora_final * 0 + minuto_final
+
+if tempo_inicial == tempo_final:
     horas = 24
     minutos = 0
-elif hora_inicial == hora_final and minuto_inicial > minuto_final:
-    horas = 23
-    minutos = 60 - minuto_inicial + minuto_final
-elif hora_inicial == hora_final and minuto_inicial < minuto_final:
-    horas = 0
-    minutos = minuto_final - minuto_inicial
-# Hora inicial < hora final
-elif hora_inicial < hora_final and minuto_inicial == minuto_final:
-    horas = hora_final - hora_inicial
-    minutos = 0
-elif hora_inicial < hora_final and minuto_inicial < minuto_final:
-    horas = hora_final - hora_inicial
-    minutos = minuto_inicial - minuto_final
-elif hora_inicial < hora_final and minuto_inicial > minuto_final:
-    horas = hora_final - (hora_inicial + 1)
-    minutos = 60 - minuto_inicial + minuto_final
+elif tempo_inicial < tempo_final:
+    tempo_jogo = tempo_final - tempo_inicial
+    horas = tempo_jogo//60
+    minutos = tempo_jogo % 60
+else:
+    tempo_jogo = 1440 - tempo_inicial + tempo_final
+    horas = tempo_jogo // 60
+    minutos = tempo_jogo % 60
 print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
