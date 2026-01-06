5c8fa566e79ca50bf02651643ddce98d867c4ec1
João Paulo Medeiros Rodrigues <jp.estudos17@gmail.com>
2026-01-06 17:19:04 -0300

    Beecrowd

---
+++ 1437 - Esquerda, Volver.py
@@ -0,0 +1,13 @@
+direcao = ["N", "O", "S", "L"]
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    comandos = str(input())
+    final = 0
+    for comando in comandos:
+        if comando == "D":
+            final -= 1
+        else:
+            final += 1
+    print(direcao[final])
