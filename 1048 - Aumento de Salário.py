2cff9f2a0d86cccf2613a781e7ba6ddda6c33371
João Paulo Medeiros Rodrigues <jp.estudos17@gmal.com>
2026-01-13 22:56:29 -0300

    Beecrowd

--- 1048 - Aumento de Salário.py
+++ 1048 - Aumento de Salário.py
@@ -3,12 +3,12 @@ if salario <= 400.00:
     percentual = 15
 elif salario <= 800.00:
     percentual = 12
-elif salario <= 1200.00:        
+elif salario <= 1200.00:
     percentual = 10
 elif salario <= 2000.00:
     percentual = 7
 else:
-    percentual = 4  
+    percentual = 4
 reajuste = salario * percentual / 100
 novo_salario = salario + reajuste
 print(f"Novo salario: {novo_salario:.2f}")
